00dfeafc0906d2aabe0854badbf5802a4d62158d
Jonathan Zhang <69145546+jonathan-d-zhang@users.noreply.github.com>
2024-09-29 22:23:23 -0400

    Create POC

---
+++ README.md
@@ -0,0 +1,14 @@
+# CIS-3296 Project Proposal POC
+
+# Build Instructions
+
+## Prerequisites
+* Rust
+* A C compiler
+
+## Running
+
+```bash
+cargo run --release
+```
+
---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use tree_sitter::Parser;
+
+fn main() {
+    let code = r#"
+    class Main {
+        public static void main(String[] args) {
+            System.out.println("Hello, world!");
+        }
+    }
+    "#;
+
+    let mut parser = Parser::new();
+    let language = tree_sitter_java::LANGUAGE;
+    parser.set_language(&language.into()).expect("Error loading Java grammar");
+
+    let tree = parser.parse(code, None).unwrap();
+
+    println!("{tree:?}");
+}
